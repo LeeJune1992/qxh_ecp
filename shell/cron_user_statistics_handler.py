7299efdb8cd415e825facb2cef92a485ee35fe07
leiqiang1230 <leiqiang1230@126.com>
2014-03-14 15:19:18 +0800

    会员客户数量统计BUG

--- shell/cron_user_statistics_handler.py
+++ shell/cron_user_statistics_handler.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     trans = conn.begin()
     try:
         #
-        sql = '''SET @@GROUP_CONCAT_MAX_LEN=9999999999;INSERT INTO `user_statistics` (tjdate,new_users,giveup_users,users,hg_users,cm_users,tj_users,city_users,xian_users,hg_user_list,cm_user_list_user_list) values(
+        sql = '''SET @@GROUP_CONCAT_MAX_LEN=9999999999;INSERT INTO `user_statistics` (tjdate,new_users,giveup_users,users,hg_users,cm_users,tj_users,city_users,xian_users,hg_user_list,cm_user_list) values(
 (DATE_FORMAT(now(),'%%Y-%%m-%%d')),(select count(*) from `user` where member_time=DATE_FORMAT(now(),'%%Y-%%m-%%d'))
 ,(select count(*) from `user_giveup` where status=1 AND DATE_FORMAT(audit_time,'%%Y-%%m-%%d')=DATE_FORMAT(now(),'%%Y-%%m-%%d'))
 ,(select count(distinct user_id) from `user` where batch_id IS NULL AND user_type=2)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 ,(select count(distinct a.user_id) from address a join `user` u on u.user_id=a.user_id where u.batch_id IS NULL AND u.user_type=2 and a.district like '%%县')
 ,(select count(distinct a.user_id) from address a join `user` u on u.user_id=a.user_id where u.batch_id IS NULL AND u.user_type=2 and (a.district IS NULL or a.district ='' or a.district like '%%区'))
 ,(select group_concat(distinct u.user_id) from `user` u join `order` o on o.user_id=u.user_id where u.batch_id IS NULL AND u.user_type=2 and datediff(now(),o.created)<=90 ORDER BY u.user_id)
-,(select group_concat(distinct u.user_id) from `user` u join `order` o on o.user_id=u.user_id where u.batch_id IS NULL AND u.user_type=2 and u.user_id not in (select user_id from `order` where datediff(now(),created)<=90) ORDER BY u.user_id)
+,(select group_concat(distinct u.user_id) from `user` u where u.batch_id IS NULL AND u.user_type=2 and u.user_id not in (select user_id from `order` where datediff(now(),created)<=90) ORDER BY u.user_id)
         )'''
         conn.execute(sql)
         trans.commit()
