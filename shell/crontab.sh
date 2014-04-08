649e641d45174ce0b74e4e4b7f07bc262a375d9e
leiqiang1230 <leiqiang1230@126.com>
2014-04-08 14:55:31 +0800

    西安库房

--- shell/crontab.sh
+++ shell/crontab.sh
@@ -6,6 +6,8 @@
 30      0       *       *       *       python2.7 /data/www/ecp/shell/cron_feihu_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 30      15       *       *       *       python2.7 /data/www/ecp/shell/cron_bj_feihu_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 30      1       *       *       *       python2.7 /data/www/ecp/shell/cron_lhyd_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
+30      15       *       *       *       python2.7 /data/www/ecp/shell/cron_xianems_stat_handler.py >> /var/log/ecp/cron_xianems$(date +\%Y\%m) 2>&1
+30      15       *       *       *       python2.7 /data/www/ecptest/shell/cron_xianems_stat_handler.py >> /var/log/ecptest/cron_xianems$(date +\%Y\%m) 2>&1
 #20      0       *       *       *       python2.7 /data/www/ecp/shell/cron_deliver_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 */1     9-23    *       *       *       python2.7 /data/www/ecp/shell/cron_order_assign_scheduler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 */1     0-23    *       *       *       python2.7 /data/www/ecp/shell/cron_per_minute_scheduler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
