5cb47c00088ad02f00243100eeff9a07580558e6
leiqiang1230 <leiqiang1230@126.com>
2014-07-25 15:28:54 +0800

    增加EMS0金额

--- shell/crontab.sh
+++ shell/crontab.sh
@@ -6,7 +6,9 @@
 30      0       *       *       *       python2.7 /data/www/ecp/shell/cron_feihu_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 30      15       *       *       *       python2.7 /data/www/ecp/shell/cron_bj_feihu_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 30      1       *       *       *       python2.7 /data/www/ecp/shell/cron_lhyd_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
-30      15       *       *       *       python2.7 /data/www/ecp/shell/cron_xianems_stat_handler.py >> /var/log/ecp/cron_xianems$(date +\%Y\%m) 2>&1
+40      13       *       *       *       python2.7 /data/www/ecp/shell/cron_xianems_stat_handler.py >> /var/log/ecp/cron_xianems$(date +\%Y\%m) 2>&1
+40      13       *       *       *       python2.7 /data/www/ecp/shell/cron_xianems0_stat_handler.py >> /var/log/ecp/cron_xianems0$(date +\%Y\%m) 2>&1
+#40      13       *       *       *       python2.7 /data/www/ecptest/shell/cron_xianems0_stat_handler.py >> /var/log/ecptest/cron_xianems0$(date +\%Y\%m) 2>&1
 30      15       *       *       *       python2.7 /data/www/ecptest/shell/cron_xianems_stat_handler.py >> /var/log/ecptest/cron_xianems$(date +\%Y\%m) 2>&1
 #20      0       *       *       *       python2.7 /data/www/ecp/shell/cron_deliver_stat_handler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
 */1     9-23    *       *       *       python2.7 /data/www/ecp/shell/cron_order_assign_scheduler.py >> /var/log/ecp/cron_$(date +\%Y\%m) 2>&1
